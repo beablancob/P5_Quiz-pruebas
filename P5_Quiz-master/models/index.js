fdba402821187d2687808c83917e58585d28e7cc
Beatriz Blanco <beablancobejar@gmail.com>
2018-05-15 19:06:23 +0200

    Add files via upload

---
+++ P5_Quiz-master/models/index.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+const options = {   logging: false, operatorsAliases: false};
+const sequelize = new Sequelize("sqlite:quizzes.sqlite",options);
+
+sequelize.define('quiz', {
+    question:{
+        type: Sequelize.STRING,
+        unique: {msg: "Esta pregunta ya existe"},
+        validate: {notEmpty:{msg: "La pregunta no puede estar vacia"}}
+    },
+    answer: {
+        type: Sequelize.STRING,
+        validate: {notEmpty: {msg: "La respuesta no puede estar vacia"}}
+    }
+});
+
+//Cuando la base de datos no está creada, la creo e inicializo asi
+
+sequelize.sync()
+.then(() => sequelize.models.quiz.count())
+.then(count => {
+    if(!count){
+    return sequelize.models.quiz.bulkCreate([
+        {question: "Capital de Italia", answer: "Roma"},
+        {question: "Capital de Francia", answer: "Paris"},
+        {question: "Capital de España", answer: "Madrid"},
+        {question: "Capital de Portugal", answer: "Lisboa"}
+    ]);
+}
+})
+.catch(error => {
+    console.log(error);
+});
+
+module.exports = sequelize;
